e4c6ada37c5e8d6c5c6b781974451f90c7e1f37e
Dylan Turner <dylantdmt@gmail.com>
2020-03-13 15:29:03 -0400

    Created water (no physics)

--- SpecialBlocks.cs
+++ SpecialBlocks.cs
@@ -117,7 +117,48 @@ namespace NewSuperChunks {
 
         public override void Init() {
             Tag = "Ladder";
+            Depth = 2;
+
             SpriteIndex = new EksedraSprite(RunningEngine.Images["ladder"], new IntRect[] { new IntRect(0, 0, 64, 64) });
+            SpriteIndex.Smooth = false;
+
+            MaskX = -20;
+            MaskY = 0;
+            MaskWidth = 52;
+            MaskHeight = 64;
+        }
+
+        public override void Draw(RenderTarget target, RenderStates states) {
+            target.Draw(SpriteIndex);
+        }
+    }
+
+    public class Water : GameObject {
+        public override void EarlyUpdate(float deltaTime) {}
+        public override void Update(float deltaTime) {}
+        public override void LateUpdate(float deltaTime) {}
+        public override void OnCollision(GameObject other) {}
+        public override void OnKeyDown(bool[] keyState) {}
+        public override void OnKeyHeld(bool[] keyState) {}
+        public override void OnKeyOff(bool[] keyState) {}
+        public override void OnKeyUp(bool[] keyState) {}
+        public override void OnTimer(int timerIndex) {}
+
+        public Water(int x, int y) {
+            X = x;
+            Y = y;
+        }
+
+        public override void Init() {
+            Tag = "Water";
+            Depth = 2;
+
+            MaskX = -32;
+            MaskY = -32;
+            MaskWidth = 64;
+            MaskHeight = 64;
+
+            SpriteIndex = new EksedraSprite(RunningEngine.Images["water"], new IntRect[] { new IntRect(0, 0, 64, 64) });
             SpriteIndex.Smooth = false;
 
             MaskX = -20;
